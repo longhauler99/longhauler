28953e1ef9595b96a394d778ce81ef91906e2806
longhauler99 <sainaofficial67@gmail.com>
2023-12-23 00:58:54 +0300

    URL Generation and Signed URLs - ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,14 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Laravel Laratips</title>
+    <style>
+        .my_border {
+            color: green;
+            background: black;
+            padding: 10px;
+            border-radius: 7px;
+        }
+    </style>
 </head>
 <body style="background: darkkhaki">
 
@@ -9,7 +17,7 @@
 <a href="{{ route('about') }}">About us</a>
 <h3 style="color: maroon">Home</h3>
 
-<div>
+<div class="my_border">
     {{ route('about', ['slug'=>'english', 'apple'=>1, 'banana'=>2]) }}
 </div>
 

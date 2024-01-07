bbbf39b62cded1dbfed298357e7b57bd93d815cf
longhauler99 <sainaofficial67@gmail.com>
2024-01-07 21:26:59 +0300

    Laravel Components - Defining components in App provider for ease of access

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
 
 @section('content')
     <div>
-        <x-Ui.alert data-id="23" class="h-22">
+        <x-alert data-id="23" class="h-22">
             <h1>The alert message from slot</h1>
-        </x-Ui.alert>
+        </x-alert>
     </div>
 @endsection

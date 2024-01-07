7f76d58bf7e42e4d574acc70c6ddb3e1d563807d
longhauler99 <sainaofficial67@gmail.com>
2024-01-07 20:58:35 +0300

    Laravel Components - Appending classes and other attributes.

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center justify-between px-6 py-4 rounded {{ $typeClass() }}" {{ $attributes }}>
+<div {{ $attributes->merge(['class' => 'flex items-center justify-between px-6 py-4 rounded '. $typeClass()]) }}>
     <div>
         {{ $slot }}
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
     <div>
-        <x-alert data-id="23">
+        <x-alert data-id="23" class="h-22">
             <h1>The alert message from slot</h1>
         </x-alert>
     </div>
